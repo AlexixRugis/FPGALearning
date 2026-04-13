f3c1a8574963dbfb310715221f76652b9d310b4f
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2026-04-13 17:50:42 +0300

    refactor

--- Calc2/arx_core/src/InsnDecodeStage.sv
+++ Calc2/arx_core/src/InsnDecodeStage.sv
@@ -28,8 +28,8 @@ module InsnDecodeStage
 
     output  logic [4:0]                 rd_out,
     output  logic [ADDR_WIDTH-1:0]      pc_out,
-    output  branch_type_t               pc_branch_type_out,
-    output  logic                       pc_branch_en_out,
+    output  branch_type_t               pc_addr_type_out,
+    output  logic                       pc_jump_en_out,
 
     output  logic                       reg_write_out,
     output  logic                       mem_to_reg_out,
@@ -50,22 +50,22 @@ module InsnDecodeStage
 
 // STAGE REGISTERS
 
-logic                                   valid_in_internal;
-logic [ADDR_WIDTH-1:0]                  pc_in_internal;
-logic [INSN_WIDTH-1:0]                  insn_in_internal;
+logic                                   m_valid;
+logic [ADDR_WIDTH-1:0]                  m_pc;
+logic [INSN_WIDTH-1:0]                  m_insn;
 
 always_ff @(posedge clk or negedge arstn) begin
     if (~arstn) begin
-        valid_in_internal <= 1'b0;
+        m_valid <= 1'b0;
     end
     else begin
         if (valid_in & ready_in) begin
-            valid_in_internal <= 1'b1;
-            pc_in_internal <= pc_in;
-            insn_in_internal <= insn_in;
+            m_valid <= 1'b1;
+            m_pc <= pc_in;
+            m_insn <= insn_in;
         end
         else if (valid_out & ready_out) begin
-            valid_in_internal <= 1'b0;
+            m_valid <= 1'b0;
         end
     end
 end
@@ -74,38 +74,38 @@ end
 
 // INSN DECODER
 
-logic [6:0]                             opcode;
-logic [4:0]                             rs_1;
-logic [4:0]                             rs_2;
-logic [4:0]                             rd;
+logic [6:0]                             m_opcode;
+logic [4:0]                             m_rs_1;
+logic [4:0]                             m_rs_2;
+logic [4:0]                             m_rd;
 
-logic [2:0]                             funct_3;
-logic [6:0]                             funct_7;
+logic [2:0]                             m_funct_3;
+logic [6:0]                             m_funct_7;
 
-logic [XLEN-1:0]                        imm_r;
-logic [XLEN-1:0]                        imm_i;
-logic [XLEN-1:0]                        imm_s;
-logic [XLEN-1:0]                        imm_b;
-logic [XLEN-1:0]                        imm_u;
-logic [XLEN-1:0]                        imm_j;
+logic [XLEN-1:0]                        m_imm_r;
+logic [XLEN-1:0]                        m_imm_i;
+logic [XLEN-1:0]                        m_imm_s;
+logic [XLEN-1:0]                        m_imm_b;
+logic [XLEN-1:0]                        m_imm_u;
+logic [XLEN-1:0]                        m_imm_j;
 
 InsnDecoder insnDecoder(
-    .insn(insn_in_internal),
+    .insn(m_insn),
 
-    .opcode(opcode),
-    .rs_1(rs_1),
-    .rs_2(rs_2),
-    .rd(rd),
+    .opcode(m_opcode),
+    .rs_1(m_rs_1),
+    .rs_2(m_rs_2),
+    .rd(m_rd),
     
-    .funct_3(funct_3),
-    .funct_7(funct_7),
+    .funct_3(m_funct_3),
+    .funct_7(m_funct_7),
     
-    .imm_r(imm_r),
-    .imm_i(imm_i),
-    .imm_s(imm_s),
-    .imm_b(imm_b),
-    .imm_u(imm_u),
-    .imm_j(imm_j)
+    .imm_r(m_imm_r),
+    .imm_i(m_imm_i),
+    .imm_s(m_imm_s),
+    .imm_b(m_imm_b),
+    .imm_u(m_imm_u),
+    .imm_j(m_imm_j)
 );
 
 // -----------
@@ -118,9 +118,9 @@ RegisterFile #(
     .clk(clk),
     .arstn(arstn),
     
-    .rs_1(rs_1),
+    .rs_1(m_rs_1),
     .out_1(rs1_out),
-    .rs_2(rs_2),
+    .rs_2(m_rs_2),
     .out_2(rs2_out),
 
     .rd(rd_in),
@@ -128,40 +128,62 @@ RegisterFile #(
     .write_data(rd_val_in)
 );
 
+logic [31:0]                            m_used_reg;
+
+always_ff @(posedge clk or negedge arstn) begin
+    if (~arstn) begin
+        m_used_reg <= '0;
+    end
+    else begin
+        if (rd_we_in) begin
+            m_used_reg[rd_in] <= 1'b0;
+        end
+        if (valid_out & ready_out & reg_write_out) begin
+            m_used_reg[rd_out] <= 1'b1;
+        end
+    end
+end
+
 // -----------
 
 // OUT ASSIGNMENTS
 
-logic [XLEN-1:0]                        imm_sel_internal;
-logic                                   reg_write_internal;
-logic                                   mem_to_reg_internal;
-logic                                   mem_op_internal;
-ls_type_t                               mem_op_type_internal;
+logic [XLEN-1:0]                        m_imm;
+logic                                   m_reg_write;
+logic                                   m_mem_to_reg;
 
-logic                                   branch_en_internal;
-logic                                   pc_branch_type_internal;
-ALU_op_t                                alu_op_internal;
-ALU_src1_t                              alu_src_1_internal;
-ALU_src2_t                              alu_src_2_internal;
+logic                                   m_mem_op;
+ls_type_t                               m_mem_op_type;
+
+logic                                   m_pc_jump_en;
+branch_type_t                           m_pc_addr_type;
+
+ALU_op_t                                m_alu_op;
+ALU_src1_t                              m_alu_src_1;
+ALU_src2_t                              m_alu_src_2;
 
 always_comb begin
-    imm_out = imm_sel_internal;
-    rd_out = rd;
-    pc_out = pc_in_internal;
-    pc_branch_type_out = pc_branch_type_internal;
+    imm_out = m_imm;
+    rd_out = m_rd;
+    pc_out = m_pc;
 
-    reg_write_out = reg_write_in_internal;
-    mem_to_reg_out = mem_to_reg_internal;
-    mem_op_type_out = mem_op_type_internal;
+    reg_write_out = m_reg_write;
+    mem_to_reg_out = m_mem_to_reg;
 
-    branch_en_out = branch_en_internal;
-    alu_op_out = alu_op_internal;
-    alu_src_1_out = alu_src_1_internal;
-    alu_src_2_out = alu_src_2_internal
+    mem_op_out = m_mem_op;
+    mem_op_type_out = m_mem_op_type;
+
+    pc_jump_en_out = m_pc_jump_en;
+    pc_addr_type_out = m_pc_addr_type;
+
+    alu_op_out = m_alu_op;
+    alu_src_1_out = m_alu_src_1;
+    alu_src_2_out = m_alu_src_2;
 end
 
 always_comb begin
-    valid_out = valid_in_internal;
+    valid_out = m_valid & (~m_used_reg[m_rs_1] & ~m_used_reg[m_rs_2]);
+    ready_in = ~m_valid | (valid_out & ready_out);
 end
 
 // -----------
@@ -169,133 +191,136 @@ end
 // DECODING
 
 always_comb begin
-    imm_sel_internal = '0;
-    reg_write_internal = 1'b0;
-    mem_to_reg_internal = 1'b0;
-    mem_op_internal = 1'b0;
-    mem_op_type_internal = LOAD_WORD;
-    branch_en_internal = 1'b0;
-    pc_branch_type_internal = BRANCH_PC_IMM;
-    alu_op_internal = IALU_ADD;
-    alu_src_1_internal = OP_SRC_RS1;
-    alu_src_2_internal = OP_SRC_RS2;
+    m_imm = '0;
+    m_reg_write = 1'b0;
+    m_mem_to_reg = 1'b0;
 
-    case (opcode)
+    m_mem_op = 1'b0;
+    m_mem_op_type = LOAD_WORD;
+
+    m_pc_jump_en = 1'b0;
+    m_pc_addr_type = BRANCH_PC_IMM;
+
+    m_alu_op = IALU_ADD;
+    m_alu_src_1 = OP_SRC_RS1;
+    m_alu_src_2 = OP_SRC_RS2;
+
+    case (m_opcode)
     7'b0110011: begin
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_RS1;
-        alu_src_2_internal = OP_SRC_RS2;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_RS1;
+        m_alu_src_2 = OP_SRC_RS2;
 
-        case ({funct_3, funct_7})
-        {3'h0, 7'h00}: alu_op_internal = IALU_ADD;
-        {3'h0, 7'h20}: alu_op_internal = IALU_SUB;
-        {3'h4, 7'h00}: alu_op_internal = IALU_XOR;
-        {3'h6, 7'h00}: alu_op_internal = IALU_OR;
-        {3'h7, 7'h00}: alu_op_internal = IALU_AND;
-        {3'h1, 7'h00}: alu_op_internal = IALU_SLL;
-        {3'h5, 7'h00}: alu_op_internal = IALU_SRL;
-        {3'h5, 7'h20}: alu_op_internal = IALU_SRA;
-        {3'h2, 7'h00}: alu_op_internal = IALU_SLT;
-        {3'h3, 7'h00}: alu_op_internal = IALU_SLTU;
+        case ({m_funct_3, m_funct_7})
+        {3'h0, 7'h00}: m_alu_op = IALU_ADD;
+        {3'h0, 7'h20}: m_alu_op = IALU_SUB;
+        {3'h4, 7'h00}: m_alu_op = IALU_XOR;
+        {3'h6, 7'h00}: m_alu_op = IALU_OR;
+        {3'h7, 7'h00}: m_alu_op = IALU_AND;
+        {3'h1, 7'h00}: m_alu_op = IALU_SLL;
+        {3'h5, 7'h00}: m_alu_op = IALU_SRL;
+        {3'h5, 7'h20}: m_alu_op = IALU_SRA;
+        {3'h2, 7'h00}: m_alu_op = IALU_SLT;
+        {3'h3, 7'h00}: m_alu_op = IALU_SLTU;
         endcase
     end
     7'b0010011: begin
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_RS1;
-        alu_src_2_internal = OP_SRC_IMM;
-        imm_sel_internal = imm_i;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_RS1;
+        m_alu_src_2 = OP_SRC_IMM;
+        m_imm = m_imm_i;
 
-        case (funct_3)
-        3'h0: alu_op_internal = IALU_ADD;
-        3'h4: alu_op_internal = IALU_XOR;
-        3'h6: alu_op_internal = IALU_OR;
-        3'h7: alu_op_internal = IALU_AND;
-        3'h1: alu_op_internal = IALU_SLL;
-        3'h5: alu_op_internal = funct_7 === 7'h00 ? IALU_SRL : IALU_SRA;
-        3'h2: alu_op_internal = IALU_SLT;
-        3'h3: alu_op_internal = IALU_SLTU;
+        case (m_funct_3)
+        3'h0: m_alu_op = IALU_ADD;
+        3'h4: m_alu_op = IALU_XOR;
+        3'h6: m_alu_op = IALU_OR;
+        3'h7: m_alu_op = IALU_AND;
+        3'h1: m_alu_op = IALU_SLL;
+        3'h5: m_alu_op = m_funct_7 === 7'h00 ? IALU_SRL : IALU_SRA;
+        3'h2: m_alu_op = IALU_SLT;
+        3'h3: m_alu_op = IALU_SLTU;
         endcase
     end
     7'b0000011: begin
-        reg_write_internal = 1'b1;
-        mem_to_reg_internal = 1'b1;
-        mem_op_internal = 1'b1;
+        m_reg_write = 1'b1;
+        m_mem_to_reg = 1'b1;
 
-        alu_src_1_internal = OP_SRC_RS1;
-        alu_src_2_internal = OP_SRC_IMM;
-        alu_op_internal = IALU_ADD;
-        imm_sel_internal = imm_i;
+        m_alu_src_1 = OP_SRC_RS1;
+        m_alu_src_2 = OP_SRC_IMM;
+        m_alu_op = IALU_ADD;
+        m_imm = m_imm_i;
 
-        case(funct_3)
-        3'h0: mem_op_type_internal = LOAD_BYTE;
-        3'h1: mem_op_type_internal = LOAD_HALFWORD;
-        3'h2: mem_op_type_internal = LOAD_WORD;
-        3'h4: mem_op_type_internal = LOAD_BYTE_UNSIGNED;
-        3'h5: mem_op_type_internal = LOAD_HALFWORD_UNSIGNED;
+        m_mem_op = 1'b1;
+        case(m_funct_3)
+        3'h0: m_mem_op_type = LOAD_BYTE;
+        3'h1: m_mem_op_type = LOAD_HALFWORD;
+        3'h2: m_mem_op_type = LOAD_WORD;
+        3'h4: m_mem_op_type = LOAD_BYTE_UNSIGNED;
+        3'h5: m_mem_op_type = LOAD_HALFWORD_UNSIGNED;
         endcase
     end
     7'b0100011: begin
-        alu_src_1_internal = OP_SRC_RS1;
-        alu_src_2_internal = OP_SRC_IMM;
-        alu_op_internal = IALU_ADD;
-        imm_sel_internal = imm_s;
+        m_alu_src_1 = OP_SRC_RS1;
+        m_alu_src_2 = OP_SRC_IMM;
+        m_alu_op = IALU_ADD;
+        m_imm = m_imm_s;
 
-        mem_op_internal = 1'b1;
-        case (funct_3)
-        3'h0: mem_op_type_internal = STORE_BYTE;
-        3'h1: mem_op_type_internal = STORE_HALFWORD;
-        3'h2: mem_op_type_internal = STORE_WORD;
+        m_mem_op = 1'b1;
+        case (m_funct_3)
+        3'h0: m_mem_op_type = STORE_BYTE;
+        3'h1: m_mem_op_type = STORE_HALFWORD;
+        3'h2: m_mem_op_type = STORE_WORD;
         endcase
     end
     7'b1100011: begin
-        pc_branch_type_internal = BRANCH_PC_IMM;
+        m_pc_add_type = BRANCH_PC_IMM;
 
-        alu_src_1_internal = OP_SRC_RS1
-        alu_src_2_internal = OP_SRC_RS2;
-        imm_sel_internal = imm_b;
+        m_alu_src_1 = OP_SRC_RS1;
+        m_alu_src_2 = OP_SRC_RS2;
+        m_imm = m_imm_b;
 
-        case (funct_3)
-        3'h0: alu_op_internal = IALU_EQ;
-        3'h1: alu_op_internal = IALU_NEQ;
-        3'h4: alu_op_internal = IALU_LT;
-        3'h5: alu_op_internal = IALU_GE;
-        3'h6: alu_op_internal = IALU_LTU;
-        3'h7: alu_op_internal = IALU_GEU;
+        case (m_funct_3)
+        3'h0: m_alu_op = IALU_EQ;
+        3'h1: m_alu_op = IALU_NEQ;
+        3'h4: m_alu_op = IALU_LT;
+        3'h5: m_alu_op = IALU_GE;
+        3'h6: m_alu_op = IALU_LTU;
+        3'h7: m_alu_op = IALU_GEU;
         endcase
     end
     7'b1101111: begin
-        branch_en_internal = 1'b1;
-        pc_branch_type_internal = BRANCH_PC_IMM;
-        imm_sel_internal = imm_j;
+        m_pc_jump_en = 1'b1;
+        m_pc_addr_type = BRANCH_PC_IMM;
+        m_imm = m_imm_j;
         
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_PC;
-        alu_src_2_internal = OP_SRC_FOUR;
-        alu_op_internal = IALU_ADD;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_PC;
+        m_alu_src_2 = OP_SRC_FOUR;
+        m_alu_op = IALU_ADD;
     end
     7'b1100111: begin
-        branch_en_internal = 1'b1;
-        pc_branch_type_internal = BRANCH_REG_IMM;
-        imm_sel_internal = imm_i;
+        m_pc_jump_en = 1'b1;
+        m_pc_addr_type = BRANCH_REG_IMM;
+        m_imm = m_imm_i;
 
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_PC;
-        alu_src_2_internal = OP_SRC_FOUR;
-        alu_op_internal = IALU_ADD;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_PC;
+        m_alu_src_2 = OP_SRC_FOUR;
+        m_alu_op = IALU_ADD;
     end
     7'b0110111: begin
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_ZERO;
-        alu_src_2_internal = OP_SRC_IMM;
-        alu_op_internal = IALU_ADD;
-        imm_sel_internal = imm_u;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_ZERO;
+        m_alu_src_2 = OP_SRC_IMM;
+        m_alu_op = IALU_ADD;
+        m_imm = m_imm_u;
     end
     7'b0010111: begin
-        reg_write_internal = 1'b1;
-        alu_src_1_internal = OP_SRC_PC;
-        alu_src_2_internal = OP_SRC_IMM;
-        alu_op_internal = IALU_ADD;
-        imm_sel_internal = imm_u;
+        m_reg_write = 1'b1;
+        m_alu_src_1 = OP_SRC_PC;
+        m_alu_src_2 = OP_SRC_IMM;
+        m_alu_op = IALU_ADD;
+        m_imm = m_imm_u;
     end
     default: begin
     end
